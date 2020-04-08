5aae6b8f27d9b44f88e9de491862e0569a9db5ca
lokman hossen <lokman@divine-it.net>
2020-04-08 11:07:07 +0600

    Handle Our First Route

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express()
 
 const PORT = process.env.PORT || 8090
+
+app.get('/', (req, res) => {
+    res.send("<h1>I am listening</h1>")
+})
+
 app.listen(PORT, ()=>{
     console.log(`Server is running on ${PORT}`)
 })
