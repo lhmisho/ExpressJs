aec89de19ffaaeb40b45246e2f17153357929377
lokman hossen <lokman@divine-it.net>
2020-04-08 21:51:52 +0600

    multiple route

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ app.get('/', (req, res) => {
     res.send("<h1>I am listening</h1>")
 })
 
+app.get('/about', (req, res) => {
+    res.send("<h1>I am about</h1>")
+})
+
+app.get('*', (req, res) => {
+    res.send("<h1>404 not found</h1>")
+})
+
+
+
 app.listen(PORT, ()=>{
     console.log(`Server is running on ${PORT}`)
 })
