3045e8bb24d2dd720eb4d31fc986e759eebf44a0
lokman hossen <lokman@divine-it.net>
2020-04-10 01:58:09 +0600

    nodemon install and middleware and custom middle ware testing

--- app.js
+++ app.js
@@ -3,11 +3,35 @@ const app = express()
 
 const PORT = process.env.PORT || 8090
 
+
+function customeMiddleWare(req, res, next){
+    if(req.url === '/help'){
+        res.send('<h1>This page is blocked by admin</h1>')
+    }
+    next()
+}
+
+function tinyLogger(){
+    return (req, res, next) =>{
+        console.log(`${req.method} -- ${req.url}`)
+        next()
+    }
+}
+
+let middlewares = [customeMiddleWare, tinyLogger()]
+app.use(middlewares)
+
 app.get('/', (req, res) => {
     res.send("<h1>I am listening</h1>")
 })
 
 app.get('/about', (req, res) => {
+    res.json({
+        message: "This is massege from about"
+    })
+})
+
+app.get('/help', (req, res) => {
     res.json({
         message: "This is massege from about"
     })
@@ -22,3 +46,4 @@ app.get('*', (req, res) => {
 app.listen(PORT, ()=>{
     console.log(`Server is running on ${PORT}`)
 })
+
---
+++ index.js
@@ -0,0 +1,3 @@
+console.log("i have installed nodemon")
+console.log("another save")
+console.log("Mon have you ever seen rain")
